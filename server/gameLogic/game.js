568713f62e7d1c2a02133f3d51290a1d6a2ca222
Sam <samuel.collis@students.plymouth.ac.uk>
2019-12-29 23:35:43 +0000

    Added some base game logic and tests

--- server/gameLogic/game.js
+++ server/gameLogic/game.js
@@ -1,8 +1,8 @@
-function generatePool() {
 
-    let tilePool = {};
+//Function used to generate pool
+function generatePool(poolOptions) {
 
-    let poolOptions = createPoolOptions();
+    let tilePool = [];
 
     for(let i = 0; i < 26; i++) {
         let letter = (i + 10).toString(36);
@@ -19,86 +19,363 @@ function generatePool() {
 
 }
 
+//Shuffle a group of tiles
+function shuffleTiles(tileGroup) {
+    return tileGroup.sort(() => Math.random() - 0.5);
+}
+
+//deals a hand
+function dealHand(handSize, pool) {
+    let hand =  [];
+
+    for(let i = 0; i < handSize; i++) {
+        hand.push(pool.pop());
+    }
+
+    return hand;
+
+}
+
+function drawTiles(pool, hand, handSize) {
+    let numberOfTiles = handSize - hand.length;
+    for(let i = 0; i < numberOfTiles; i++) {
+        hand.push(pool.pop());
+    }
+    return hand;
+}
+
+//Function used to define the default pool options
 function createPoolOptions() {
     let poolOptions = {
-        aCount = 9,
-        aValue = 1,
+        aCount: 9,
+        aValue: 1,
     
-        bCount = 2,
-        bValue = 3,
+        bCount: 2,
+        bValue: 3,
     
-        cCount = 2,
-        cValue = 3,
+        cCount: 2,
+        cValue: 3,
     
-        dCount = 4,
-        dValue = 2,
+        dCount: 4,
+        dValue: 2,
     
-        eCount = 1,
-        eValue = 1,
+        eCount: 12,
+        eValue: 1,
     
-        fCount = 2,
-        fValue = 4,
+        fCount: 2,
+        fValue: 4,
     
-        gCount = 3,
-        gValue = 2,
+        gCount: 3,
+        gValue: 2,
     
-        hCount = 2,
-        hValue = 4,
+        hCount: 2,
+        hValue: 4,
     
-        iCount = 9,
-        iValue = 1,
+        iCount: 9,
+        iValue: 1,
     
-        jCount = 1,
-        jValue = 8,
+        jCount: 1,
+        jValue: 8,
     
-        kCount = 1,
-        kValue = 5,
+        kCount: 1,
+        kValue: 5,
     
-        lCount = 4,
-        lValue = 1,
+        lCount: 4,
+        lValue: 1,
     
-        mCount = 2,
-        mValue = 3,
+        mCount: 2,
+        mValue: 3,
     
-        nCount = 6,
-        nValue = 1,
+        nCount: 6,
+        nValue: 1,
     
-        oCount = 8,
-        oValue = 1,
+        oCount: 8,
+        oValue: 1,
     
-        pCount = 2,
-        pCount = 3,
+        pCount: 2,
+        pCount: 3,
     
-        qCount = 1,
-        qValue = 1,
+        qCount: 1,
+        qValue: 1,
     
-        rCount = 6,
-        rValue = 1,
+        rCount: 6,
+        rValue: 1,
     
-        sCount = 4,
-        sValue = 1,
+        sCount: 4,
+        sValue: 1,
     
-        tCount = 6,
-        tValue = 1,
+        tCount: 6,
+        tValue: 1,
     
-        u,ount = 4,
-        uValue = 1,
+        uCount: 4,
+        uValue: 1,
     
-        vCount = 2,
-        vValue = 4,
+        vCount: 2,
+        vValue: 4,
     
-        wCount = 2,
-        wValue = 4,
+        wCount: 2,
+        wValue: 4,
     
-        xCount = 1,
-        xValue = 8,
+        xCount: 1,
+        xValue: 8,
     
-        yCount = 2,
-        yValue = 4,
+        yCount: 2,
+        yValue: 4,
     
-        zCount = 1,
-        zValue = 1
+        zCount: 1,
+        zValue: 10,
     };
 
     return poolOptions
 }
+
+//create the board
+function createBoard() {
+	//Define the board array
+    let board = [];
+	//define the size
+    let boardSize = 15;
+    
+	//Define the tiles
+    let wordx2 = {type: 'word', multiplier: 2, tile: {}, used: false};
+    let wordx3 = {type: 'word', multiplier: 3, tile: {}, used: false};
+
+    let letterx2 = {type: 'letter', multiplier: 2, tile: {}, used: false};
+    let letterx3 = {type: 'letter', multiplier: 3, tile: {}, used: false};
+
+    let normal = {type: 'none', multiplier: 0, tile: {}, used: false};
+    let centre = {type: 'centre', multiplier: 0, tile: {}, used: false};
+
+	//Create board
+    for(let i = 0; i < boardSize; i++) {
+        //Create a row
+        let row = [];
+
+        for(let j = 0; j < boardSize; j++) {
+            if((i == 0 || i == 14) && (j == 0 || j == 7 || j == 14) 
+            || i == 7 && (j == 0 || j == 14)) {
+                row.push(JSON.parse(JSON.stringify(wordx3)));
+            } else if ((i == 1 || i == 13) && (j == 1 || j == 13)
+            || (i == 2 || i == 12) && (j == 2 || j == 12)
+            || (i == 3 || i == 11) && (j == 3 || j == 11) 
+            || (i == 4 || i == 10) && (j == 4 || j == 10)) {
+                row.push(JSON.parse(JSON.stringify(wordx2)));
+            } else if ((i == 0 || i == 14)&& (j == 3 || j == 11)
+            || (i == 2 || i == 12) && (j == 6 || j == 8)
+            || (i == 3 || i == 11) && (j == 0 || j == 7 || j == 14)
+            || (i == 6 || i == 8) && (j == 2 || j == 6 || j == 8 || j == 12)
+            || i == 7 && (j == 3 || j == 11)) {
+                row.push(JSON.parse(JSON.stringify(letterx2)));
+            } else if ((i == 1 || i == 13) && (j == 5 || j == 9)
+            || (i == 5 || i == 9) && (j == 1 || j == 5 || j == 9 || j == 13)) {
+                row.push(JSON.parse(JSON.stringify(letterx3)));
+            } else if (i == 7 && j == 7) {
+                row.push(JSON.parse(JSON.stringify(centre)));
+            } else {
+                row.push(JSON.parse(JSON.stringify(normal)));
+            }
+        }
+
+        board.push(row);
+    }
+
+    return board;
+}
+
+function initialSetup(players, handSize) {
+    let game = {};
+
+    let options = createPoolOptions();
+    let pool = generatePool(options);
+    shuffleTiles(pool);
+
+    let hands = [];
+
+    players.forEach(player => {
+        let hand = dealHand(handSize, pool);
+        hands.push(hand);
+    });
+
+    let board = createBoard();
+
+    game.players = players;
+    game.hands = hands;
+    game.board = board;
+    game.pool = pool;
+    game.activePlayer = 0;
+    game.firstTurn = true;
+
+    return game;
+}
+
+function placeTile(game, tile, x, y)  {
+    if (checkPlacement(game, tile, x, y)) {
+        game.board[x][y].tile = tile;
+    }
+}
+
+function checkPlacement(game, tile, x ,y) {
+    // If the tile in the board is an empty object, then it's free
+    if (JSON.stringify(game.board[x][y].tile) == '{}') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkIfTileExists(boardTile) {
+    return JSON.stringify(boardTile.tile) != '{}';
+}
+
+function checkMove(game, tiles, moves) {
+    //clone the game
+    let newGame = JSON.parse(JSON.stringify(game));
+
+    //if it's the first turn, the centre
+    //square must be used
+    if(game.firstTurn) {
+        let centreUsed = false;
+
+        for(let i = 0; i < moves.length; i++) {
+            if (moves[i].x == 7 && moves[i].y == 7) {
+                centreUsed = true;
+            }
+        }
+
+        if (!centreUsed) {
+            return false
+        }
+    }
+
+
+    //We should check to see if positions are placeable
+    for(let i = 0; i < tiles.length; i++) {
+        let tile = tiles[i];
+        let move = moves[i];
+
+        if (!checkPlacement(newGame, tile, move.x, move.y)) {
+            return false;
+        } 
+        
+    }
+   
+    //Check to see if moves are in a single line
+    var xVals = [];
+    var yVals = [];
+
+    //Collect all x and y values
+    for(let i = 0; i < moves.length; i++) {
+        xVals.push(moves[i].x);
+        yVals.push(moves[i].y);
+    }
+
+
+    //If the size of both is not one, then we have tiles
+    //not placed in a horizontal or vertical line
+    if (new Set(xVals).size != 1 && new Set(yVals).size != 1) {
+        return false;
+    }
+
+    let moveIsVertical = (new Set(xVals).size == 1);
+    
+
+    if (moveIsVertical) {
+        yVals.sort(function(a, b){return a - b});
+
+        for(let i = 1; i < yVals.length; i++) {
+            if(yVals[i] - yVals[i - 1] != 1) {
+                let missingVal = yVals[i] - 1;
+                if (!checkIfTileExists(newGame.board[xVals[0]][missingVal])) {
+                    return false;
+                }
+            }
+        }
+
+    } else {
+        xVals.sort(function(a, b){return a - b});
+        for(let i = 1; i < xVals.length; i++) {
+            if(xVals[i] - xVals[i - 1] != 1) {
+                let missingVal = xVals[i] - 1;
+                if (!checkIfTileExists(newGame.board[missingVal][yVals[0]])) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    //If we get to here, then we want to place the tiles and make
+    //sure that it has joined on to something
+    for(let i = 0; i < moves.length; i++){
+        placeTile(newGame, tiles[i], moves[i].x, moves[i].y);
+    }
+
+    let validMove = true;
+
+    //Check that everything has atleast 1 adjacent tile
+    for (let i = 0; i < newGame.board.length; i++) {
+        for (let j = 0; j < newGame.board[0].length; j++) {
+            if (JSON.stringify(game.board[i][j].tile) != '{}') {
+
+                let toCheck = [];
+                //up
+                toCheck.push({x : i, y: j + 1});
+                //down
+                toCheck.push({x : i, y: j - 1});
+                //left
+                toCheck.push({x : i - 1, y: j});
+                //right
+                toCheck.push({x : i + 1, y: j});
+
+                //Check to make sure all positions are posible tile positions
+                for (let k = toCheck.length - 1; k >= 0; k--) {
+                    if (toCheck[k].x < 0 || toCheck[k].x == 15 
+                        || toCheck[k].y < 0 || toCheck[k].y == 15) {
+                            toCheck.splice(k, 1);
+                    } 
+                }
+
+
+                let validPosition = false;
+
+                for(let k = 0; k < toCheck.length; k++) {
+
+                    if(JSON.stringify(newGame.board[toCheck[k].x][toCheck[k].y].tile) != '{}') {
+                        validPosition = true;
+                    } 
+                    
+                    if (validPosition) {
+                        break;
+                    }
+                }
+
+                if (!validPosition) {
+                    validMove = false;
+                }
+
+            }
+
+            if (!validMove) {
+                break;
+            }
+
+        }
+        if (!validMove) {
+            break;
+        }
+    }
+
+    return validMove;
+
+}
+
+
+module.exports.generatePool = generatePool;
+module.exports.createPoolOptions = createPoolOptions;
+module.exports.shuffleTiles = shuffleTiles;
+module.exports.dealHand = dealHand;
+module.exports.drawTiles = drawTiles;
+module.exports.createBoard = createBoard;
+module.exports.initialSetup = initialSetup;
+module.exports.placeTile = placeTile;
+module.exports.checkPlacement = checkPlacement;
+module.exports.checkMove = checkMove;
--- server/gameLogic/test.js
+++ server/gameLogic/test.js
@@ -1,12 +1,195 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 
-describe('Game Test', () => {
+var game = require('./game');
 
-    it('dummy test', (done) => {
-        console.log('dummy');
-        expect(2).equal(2);
+
+describe('Game Logic Test', () => {
+
+    it('The game should be able to produce default pool options', (done) => {
+        console.log('creating default pool options');
+        let options = game.createPoolOptions();
+        expect(options.zValue).equal(10);
+        expect(options.fValue).equal(4);
         done();
+    });
+
+    it('The game should be able to generate a tile pool based on default options', (done) => {
+        console.log('creating default tile pool');
+        let pool = game.generatePool(game.createPoolOptions()); 
+        expect(pool.length).equal(99);
+        expect(pool[0].letter).equal('a');
+        done();
+    });
+
+    it('The game should be able to shuffle a group of tiles', (done) => {
+        console.log('creating default pool');
+        let pool = game.generatePool(game.createPoolOptions());
+        console.log('shuffling said pool');
+        //We use parse and stringify to clone an item
+        let shuffled = game.shuffleTiles(JSON.parse(JSON.stringify(pool)));
+
+        expect(pool).not.equal(shuffled, "Pool and shuffled is the same");
+        expect(pool.length).equal(shuffled.length, "The two arrays were not the same length");
+        done();
+
+    });
+
+    it('The game should be able to deal a hand of X size', (done) => {
+        console.log('Creating hand');
+        let pool = game.generatePool(game.createPoolOptions());
+       
+        let poolSize = pool.length;
+        let handSize = 7;
+
+        let hand = game.dealHand(handSize, pool);
+
+        expect(poolSize - handSize).equal(pool.length);
+        expect(hand.length).equal(handSize);
+        
+        console.log('Created hand:');
+        console.log(JSON.stringify(hand));
+        done();
+    });
+
+    it('The game should be able to allow the player draw X number of tile', (done) => {
+        let pool = game.generatePool(game.createPoolOptions());
+        let handSize = 8;
+
+        let hand = game.dealHand(handSize, pool);
+
+        let poolSize = pool.length;
+        let discardCount = Math.floor(Math.random() * 9);
+        console.log("Discarding: " + discardCount);
+
+        //If we just remove the tiles from the hand, we can simulate placing them for now
+        for(let i = 0; i < discardCount; i++) {
+            hand.pop();
+        }
+
+        console.log(JSON.stringify(hand));
+
+        console.log("hand size was " + handSize + " now is:" + hand.length);
+
+        game.drawTiles(pool, hand, handSize);
+
+        console.log("hand after drawing  tiles");
+        console.log(JSON.stringify(hand));
+
+        expect(hand.length).equal(8);
+        expect(pool.length).equal(poolSize - discardCount);
+        done();
+    });
+
+    it('The game should be able to create the game board', (done) => {
+
+        let board = game.createBoard();
+
+        //The standard scrabble board is 15x15
+        //so check x
+        expect(board.length).equal(15);
+        //check y
+        expect(board[0].length).equal(15);
+
+        expect(board[0][0].type).equal('word');
+        expect(board[0][0].multiplier).equal(3);
+        expect(board[1][1].type).equal('word');
+        expect(board[1][1].multiplier).equal(2);
+        expect(board[5][1].type).equal('letter');
+        expect(board[5][1].multiplier).equal(3);
+        expect(board[7][7].type).equal('centre');
+        expect(board[7][7].multiplier).equal(0);
+
+        done();
+    });
+
+    it('The game should be able to be setup', (done) => {
+        let players = [1,2,3];
+        let setup = game.initialSetup(players, 8);
+
+        expect(setup.players.length).equal(players.length);
+        expect(setup.hands.length).equal(players.length);
+        expect(setup.pool.length).equal((99 - 24));
+        done();
+    });
+
+    it('The game shoud allow you to check if a tile placement valid is correct', (done) => {
+        let players = [1,2,3];
+        let setup = game.initialSetup(players, 8);
+
+        //Initially the square is empty so you should be able to
+        let success = game.checkPlacement(setup, setup.hands[0], 0,0);
+        game.placeTile(setup, setup.hands[0], 0,0);
+        expect(success).equal(true);
+
+        //then as it's got something in the place it shouldn't be valid
+        success = game.checkPlacement(setup, setup.hands[0], 0,0);
+        expect(success).equal(false);
+
+        done();
+    });
+
+    it('The game should allow you to place a tile', (done) => {
+        let players = [1,2];
+        let setup = game.initialSetup(players, 8);
+
+        game.placeTile(setup, {letter: 'e', value: 3}, 0,0);
+
+        let affectedTiles = 0;
+
+        for(let i = 0; i < setup.board.length; i++) {
+            for(let j = 0; j < setup.board[0].length; j++) {
+                if (JSON.stringify(setup.board[i][j].tile) != '{}') {
+                    affectedTiles++;
+                }
+            }
+        }
+
+        expect(affectedTiles).equal(1);
+
+        done();
+
     })
+
+    it('The game should be able to check if a move is valid (tile placement wise)', (done) => {
+        let players = [1,2];
+        let setup = game.initialSetup(players, 8);
+
+        let tiles = [];
+        tiles.push({letter: 'e', value: 3});
+        tiles.push({letter: 'e', value: 3});
+        tiles.push({letter: 'e', value: 3});
+        tiles.push({letter: 'e', value: 3});
+
+        let moves = [];
+        moves.push({x: 7, y: 7});
+        moves.push({x: 8, y: 7});
+        moves.push({x: 9, y: 7});
+        moves.push({x: 10, y: 7});
+
+        let isValid = game.checkMove(setup, tiles, moves);
+
+        expect(isValid).equal(true)
+
+        moves = [];
+        moves.push({x: 7, y: 7});
+        moves.push({x: 9, y: 7});
+        moves.push({x: 10, y: 7});
+        moves.push({x: 11, y: 7});
+
+        isValid = game.checkMove(setup, tiles, moves);
+        expect(isValid).equal(false);
+
+        game.placeTile(setup, {letter: 'e', value: 3}, 8,7);
+        isValid = game.checkMove(setup, tiles, moves);
+        expect(isValid).equal(true);
+
+        done();
+
+    });
+
+
+
+
 
 })
