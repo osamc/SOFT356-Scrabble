705dc405a5f05f4041533a5af9063372deee1c36
Sam <samuel.collis@students.plymouth.ac.uk>
2019-12-30 00:28:34 +0000

    updated package.json to remove post install and to try and fix testing issues on jenkins

--- server/schemas.js
+++ server/schemas.js
@@ -7,33 +7,16 @@ var player = mongoose.model('player', {
     activeRoomId: String
 });
 
-const gameTile = class {
-    constructor(letter, value) {
-        this.letter = letter;
-        this.value = value;
-    }
-}
 
-var gameTile = mongoose.Schema('gameTile' , {
-    letter: String,
-    value: Number,
-});
-
-var hand = mongoose.Schema('hand', {
-    id: String,
-    hand: [gameTile]
-})
 
 var room = mongoose.model('room', {
     id: String,
     messages: [String],
     maxPlayers: Number,
     players: [String],
-    tilePool: [gameTile],
-    hands: [hand]
 });
 
 
 
 module.exports.player = player;
-module.exports.room = room;
+module.exports.room = room; 
